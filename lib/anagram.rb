c8c42fa48c6b501538d1b1136ca464d1387a8b26
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-09 22:46:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ def Anagram
   end  
   
   def self.match(word_arr)
-    new_arr = @word.split("").sort
+    new_arr = self.split("").sort
     word_arr.each do |word|
       new_word = word.split("").sort
         if new_word == new_arr
