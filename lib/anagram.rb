56f0438a4702df145f8b61f73d858f385687f2e6
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-09 22:40:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+
+def Anagram(word)
+  attr_accessor :word
+  
+  def initialize(word)
+    :word = word
+  end  
+  
+  def match
+    new_arr = @word.split(//).sort
+    
+    
+  end
+  
+  
+  
+  
+end
